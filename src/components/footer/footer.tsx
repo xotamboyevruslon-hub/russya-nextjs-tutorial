d59709da80fdeca34756af66ccdf386c6fb7e91a
Ruslonbek2008 <xotamboyevruslonbek@gmail.com>
2025-11-23 22:01:49 +0500

    Hero components

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,7 +7,7 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Footer = () => {
   return (
-    <Box padding={'20px'} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: "primary.main", color: 'white' }}
+    <Box padding={'20px'} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#141414', color: 'white' }}
     >
       <Typography>{format(new Date(), 'yyyy')} Ruslon. All Right Reserved.</Typography>
       <Box sx={{ display: 'flex', gap: '15px' }}>
