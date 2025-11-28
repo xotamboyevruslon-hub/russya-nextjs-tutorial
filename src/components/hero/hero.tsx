7398914eba72a98853ef76990115fbabd206cd88
Ruslonbek2008 <xotamboyevruslonbek@gmail.com>
2025-11-28 07:59:24 +0500

    Content component

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -20,9 +20,9 @@ const Hero = () => {
                         <Box sx={{ position: 'relative', width: '100%', height: '70vh' }}>
                             <Image src={item.image} alt={item.title} fill style={{ objectFit: 'cover' }} />
                             <Box sx={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0, .6)' }} />
-                            <Box width={{ xs: '100%', sm: '70%' }} position={'relative'} color={'white'} sx={{ top: '50%', transform: 'translateY(-50%)', paddingLeft: { xs: '10px', sm: '50px' } }} zIndex={999}>
-                                <Typography variant='h2'>{item.title}</Typography>
-                                <Typography variant='h5'>{item.exerpt}</Typography>
+                            <Box width={{ xs: '100%', md: '70%' }} position={'relative'} color={'white'} sx={{ top: '50%', transform: 'translateY(-50%)', paddingLeft: { xs: '10px', md: '50px' } }} zIndex={999}>
+                                <Typography sx={{ fontSize: { xs: '30px', md: '50px' } }}>{item.title}</Typography>
+                                <Typography color={'gray'} sx={{ fontSize: { xs: '20px', md: '25px' } }}>{item.exerpt}</Typography>
                                 <Box sx={{ display: 'flex', gap: '10px', marginTop: '20px' }}>
                                     <Avatar alt={item.author.name} src={item.author.image} />
                                     <Box>
